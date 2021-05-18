3dd4928ca38030361dc0e8aa7dee381739f63a50
Kasey Schwartz <kcmjschwartz@gmail.com>
2021-05-18 14:21:46 -0400

    new country fetch and load dropdown

--- public/scripts.js
+++ public/scripts.js
@@ -1,6 +1,6 @@
 
-const baseUrl = 'https://calendarific.com/api/v2/holidays';
-const key = '374a137e66a0737672e2816b140a3fdf0be28d3b';
+const baseUrl1 = 'https://calendarific.com/api/v2/holidays';
+const key1 = '374a137e66a0737672e2816b140a3fdf0be28d3b';
 const searchForm = document.querySelector('form');
 const resultsInfo = document.querySelector('.resultsTable')
 const globe = document.querySelector('.resultsSection')
@@ -13,9 +13,38 @@ searchForm.addEventListener('submit', fetchHolidayInfo);
 
 resultsInfo.style.visibility = "hidden";
 
+
+async function loadCountries( ){
+    let response1 = await fetch('https://restcountries.eu/rest/v2/all');
+    console.log(response1);
+    let countriesReturned = await response1.json()
+    console.log(countriesReturned);
+    displayCountries(countriesReturned);
+}
+loadCountries();
+
+function displayCountries(countriesReturned){
+    let countryInfo = countriesReturned;
+    console.log(countryInfo);
+    for(let i = 0; i < countryInfo.length; i++) {
+        let countryName = countryInfo[i].name;
+        let countryCode = countryInfo[i].alpha2Code;
+
+        let countryOption = document.createElement('option');
+
+        countryOption.value =`${countryCode}`
+        countryOption.innerHTML = countryName
+
+        enteredCountry.appendChild(countryOption);    
+    }};
+
+
+    
+
+
 async function fetchHolidayInfo(e){
     e.preventDefault();
-    let url = baseUrl+'?api_key='+key+'&country='+enteredCountry.value+'&year='+enteredYear.value+'&type='+enteredType.value;
+    let url = baseUrl1+'?api_key='+key1+'&country='+enteredCountry.value+'&year='+enteredYear.value+'&type='+enteredType.value;
     console.log(url);
     let response = await fetch(url);
     console.log(response);
